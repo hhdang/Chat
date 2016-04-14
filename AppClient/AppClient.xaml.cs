ffec3c6090305fec99864a402e4158f3aba02279
zhanglei <lei.zhang@sunscell.com>
2016-04-14 16:20:11 +0800

    first commit

---
+++ AppClient/AppClient.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string address = "192.168.1.124";
+        public int port = 8008;
+        public string separator = "{|}";
+        public bool isConnect = false;
+        public Socket client = null;
+        public string userName = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtAddr.Text = address;
+            txtPort.Text = port.ToString();
+
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isConnect)
+            {
+                MessageBox.Show("请登陆后在发言");
+                return;
+            }
+
+
+            string msg = userName + "说:" + txtSend.Text;
+
+            SendToServer(MsgType.CHAT, msg);
+
+        }
+
+        public Socket ConnectServer()
+        {
+            if (isConnect) return client;
+            
+
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ip = IPAddress.Parse(address);
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+
+            isConnect = true;
+
+            return client;
+        }
+
+        public void AddMsg(string msg)
+        {
+            if (msg.IndexOf(separator) == -1)
+            {
+                Debug.WriteLine(msg + "-----------");
+                return;
+            }
+            string[] info = msg.Split(separator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            string head = info[0];
+            string content = info[1];
+            switch (head)
+            {
+                case "CONNECT":
+                    RenderMsg(content + "加入聊天室", Brushes.Gray);
+                    break;
+                case "PRIVATE":
+                    RenderMsg(content, Brushes.Blue);
+                    break;
+                case "CHAT":
+                    RenderMsg(content, Brushes.Black);
+                    break;
+                case "EXIT":
+                    RenderMsg(content, Brushes.Gray);
+                    break;
+                case "LIST":
+                    //AddUser(content);
+                    break;
+                case "QUIT":
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public void RenderMsg(string msg, Brush color)
+        {
+            TextBlock tb = new TextBlock { Text = msg, Foreground = color };
+            spMsg.Children.Add(tb);
+        }
+
+        public void SendToServer(MsgType type, string msg)
+        {
+            if (client == null || client.Connected == false)
+            {
+                MessageBox.Show("未连接到服务器");
+                return;
+            }
+
+            string msgType = Enum.GetName(type.GetType(), type);
+            string tmp = msgType + separator + msg;
+            byte[] data = Encoding.Default.GetBytes(msgType + separator + msg);
+
+            client.Send(data);
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("请输入用户名");
+                return;
+            }
+            userName = txtName.Text;
+            ConnectServer();
+            SendToServer(MsgType.CONNECT, txtName.Text);
+
+            //持续接受服务端的数据
+            new Thread(() => {
+                string str = "";
+
+                while (isConnect)
+                {
+                    if (client == null || !client.Connected) break;
+                    byte[] buffer = new byte[256];
+                    int length = client.Receive(buffer);
+                    str += Encoding.Default.GetString(buffer, 0, length);
+
+
+                    if (client.Available == 0)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            Debug.WriteLine(str);
+                            AddMsg(str);
+                        });
+
+                        str = "";
+                    }
+                }
+            }).Start();
+
+            
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            if (client != null && client.Connected)
+            {
+                SendToServer(MsgType.EXIT, userName + "离开聊天室");
+                isConnect = false;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (client != null && client.Connected)
+            {
+                SendToServer(MsgType.EXIT, userName + "离开聊天室");
+                isConnect = false;
+            }
+        }
+        
+    }
+
+    public enum MsgType
+    {
+        CONNECT = 1,
+        PRIVATE,
+        CHAT,
+        EXIT,
+        LIST,
+        QUIT
+    }
+}
---
+++ AppServer/AppServer.xaml.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+using System.Diagnostics;
+
+namespace AppServer
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string separator = "{|}";
+        public Socket server = null;
+        public string address = "192.168.1.124";
+        public int port = 8008;
+        public bool isStart = false;
+        byte[] data = new byte[256]; 
+        public Hashtable clients = new Hashtable();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtPort.Text = port.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPort.Text))
+            {
+                MessageBox.Show("端口不能为空");
+                return;
+            }
+            StartUp();
+            Dispatcher.Invoke(() => {
+                RenderMsg("服务已开启...", Brushes.Gray);
+            });
+
+            //开启新线程 避免阻塞主线程
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    Socket socket = server.Accept();
+                    Debug.WriteLine("客户端连接........");
+
+                    string tmp = "";
+
+                    //接受一个新的客户端 开启新线程
+                    new Thread(() =>
+                    {
+                        //持续接受客户端信息
+                        while (true)
+                        {
+                            if (!socket.Connected) break;
+                            int length = socket.Receive(data);
+                            tmp += Encoding.Default.GetString(data, 0, length);
+
+                            //判断是否还有数据流需要接受
+                            if (socket.Available == 0)
+                            {
+                                Debug.WriteLine(tmp);
+                                Dispatcher.Invoke(() =>
+                                {
+                                    AddMsg(tmp, socket);
+                                    Boardcast(tmp);
+                                });
+                                tmp = "";
+
+                            }
+                        }
+
+                    }).Start();
+                }
+            }).Start();
+
+        }
+
+        public void Boardcast(string tmp)
+        {
+            foreach (DictionaryEntry item in clients)
+            {
+                Socket socket = (Socket)item.Key;
+                socket.Send(Encoding.Default.GetBytes(tmp));
+            }
+        }
+
+
+        public Socket StartUp()
+        {
+            if (isStart) return server;
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ip = IPAddress.Parse(address);
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+           
+            server.Bind(endPoint);
+            server.Listen(10);
+            isStart = true;
+
+            return server;
+        }
+
+        public void AddMsg(string msg, Socket socket)
+        {
+            string[] info = msg.Split(separator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            string head = info[0];
+            string content = info[1];
+            switch (head)
+            {
+                case "CONNECT":
+                    clients.Add(socket, content);
+                    RenderMsg(content + "加入聊天室", Brushes.Gray);
+                    break;
+                case "PRIVATE":
+                    RenderMsg(content, Brushes.Blue);
+                    break;
+                case "CHAT":
+                    RenderMsg(content, Brushes.Black);
+                    break;
+                case "EXIT":
+                    clients.Remove(socket);
+                    RenderMsg(content, Brushes.Gray);
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                    break;
+                case "LIST":
+                    AddUser(content);
+                    break;
+                case "QUIT":
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+        public void RenderMsg(string msg, Brush color)
+        {
+            TextBlock tb = new TextBlock { Text = msg, Foreground = color };
+            spMsg.Children.Add(tb);
+        }
+
+        public void AddUser(string name)
+        {
+            TextBlock tb = new TextBlock { Text = name };
+            spUsers.Children.Add(tb);
+        }
+
+    }
+
+
+    public enum MsgType
+    {
+        CONNECT = 1,
+        PRIVATE,
+        CHAT,
+        EXIT,
+        LIST,
+        QUIT
+    }
+}
